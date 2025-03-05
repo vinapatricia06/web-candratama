f171572fd5dab8426f84915cadc42692f48827e1
vinapatricia06 <vinapatricia2017@gmail.com>
2025-03-05 13:27:24 +0700

    omset akses

--- routes/web.php
+++ routes/web.php
@@ -32,39 +32,42 @@ Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edi
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
-// CMS Omset
-Route::get('/omsets', [OmsetController::class, 'index'])->name('omsets.index');
-Route::get('/omsets/create', [OmsetController::class, 'create'])->name('omsets.create');
-Route::post('/omsets', [OmsetController::class, 'store'])->name('omsets.store');
-Route::get('/omsets/{omset}/edit', [OmsetController::class, 'edit'])->name('omsets.edit');
-Route::put('/omsets/{omset}', [OmsetController::class, 'update'])->name('omsets.update');
-Route::delete('/omsets/{omset}', [OmsetController::class, 'destroy'])->name('omsets.destroy');
 
-Route::get('/omsets/rekap', [OmsetController::class, 'rekapBulanan'])->name('omsets.rekap');
-Route::get('omsets/export', [OmsetController::class, 'exportToExcel'])->name('omsets.export');
-Route::get('/omset/download-pdf', [OmsetController::class, 'downloadPDF'])->name('omset.download-pdf');
-Route::post('/omset/upload-chart', [OmsetController::class, 'uploadChart'])->name('omset.upload-chart');
+Route::middleware(['auth', 'role:marketing,interior_consultan,finance,'])->group(function () {
+    Route::get('/omsets', [OmsetController::class, 'index'])->name('omsets.index');
+    Route::get('/omsets/create', [OmsetController::class, 'create'])->name('omsets.create');
+    Route::post('/omsets', [OmsetController::class, 'store'])->name('omsets.store');
+    Route::get('/omsets/{omset}/edit', [OmsetController::class, 'edit'])->name('omsets.edit');
+    Route::put('/omsets/{omset}', [OmsetController::class, 'update'])->name('omsets.update');
+    Route::delete('/omsets/{omset}', [OmsetController::class, 'destroy'])->name('omsets.destroy');
 
-// CMS Progress Project
-Route::get('/progress_projects', [ProgressProjectController::class, 'index'])->name('progress_projects.index');
-Route::get('/progress_projects/create', [ProgressProjectController::class, 'create'])->name('progress_projects.create');
-Route::post('/progress_projects', [ProgressProjectController::class, 'store'])->name('progress_projects.store');
-Route::get('/progress_projects/{progressProject}/edit', [ProgressProjectController::class, 'edit'])->name('progress_projects.edit');
-Route::put('/progress_projects/{progressProject}', [ProgressProjectController::class, 'update'])->name('progress_projects.update');
-Route::delete('/progress_projects/{progressProject}', [ProgressProjectController::class, 'destroy'])->name('progress_projects.destroy');
-Route::get('/progress-projects/download', [ProgressProjectController::class, 'downloadPdf'])->name('progress_projects.downloadPdf');
-Route::post('progress_projects/hapusBulan', [ProgressProjectController::class, 'hapusBulan'])->name('progress_projects.hapusBulan');
+    Route::get('/omsets/rekap', [OmsetController::class, 'rekapBulanan'])->name('omsets.rekap');
+    Route::get('omsets/export', [OmsetController::class, 'exportToExcel'])->name('omsets.export');
+    Route::get('/omset/download-pdf', [OmsetController::class, 'downloadPDF'])->name('omset.download-pdf');
+    Route::post('/omset/upload-chart', [OmsetController::class, 'uploadChart'])->name('omset.upload-chart');
+});
 
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/progress_projects/create', [ProgressProjectController::class, 'create'])->name('progress_projects.create');
+    Route::post('/progress_projects', [ProgressProjectController::class, 'store'])->name('progress_projects.store');
+    Route::get('/progress_projects/{progressProject}/edit', [ProgressProjectController::class, 'edit'])->name('progress_projects.edit');
+    Route::put('/progress_projects/{progressProject}', [ProgressProjectController::class, 'update'])->name('progress_projects.update');
+    Route::delete('/progress_projects/{progressProject}', [ProgressProjectController::class, 'destroy'])->name('progress_projects.destroy');
+    Route::get('/progress-projects/download', [ProgressProjectController::class, 'downloadPdf'])->name('progress_projects.downloadPdf');
+    Route::post('progress_projects/hapusBulan', [ProgressProjectController::class, 'hapusBulan'])->name('progress_projects.hapusBulan');
+});
+    Route::get('/progress_projects', [ProgressProjectController::class, 'index'])->name('progress_projects.index');
 
-
+Route::middleware(['auth', 'role:admin,marketing'])->group(function () {
+    Route::get('/maintenances/create', [MaintenanceController::class, 'create'])->name('maintenances.create');
+    Route::post('/maintenances', [MaintenanceController::class, 'store'])->name('maintenances.store');
+    Route::get('/maintenances/{maintenance}/edit', [MaintenanceController::class, 'edit'])->name('maintenances.edit');
+    Route::put('/maintenances/{maintenance}', [MaintenanceController::class, 'update'])->name('maintenances.update');
+    Route::delete('/maintenances/{maintenance}', [MaintenanceController::class, 'destroy'])->name('maintenances.destroy');
+    Route::get('/maintenances/download', [MaintenanceController::class, 'downloadPdf'])->name('maintenances.downloadPdf');
+    Route::post('maintenances/hapusBulan', [MaintenanceController::class, 'hapusBulan'])->name('maintenances.hapusBulan');
+});
 Route::get('/maintenances', [MaintenanceController::class, 'index'])->name('maintenances.index');
-Route::get('/maintenances/create', [MaintenanceController::class, 'create'])->name('maintenances.create');
-Route::post('/maintenances', [MaintenanceController::class, 'store'])->name('maintenances.store');
-Route::get('/maintenances/{maintenance}/edit', [MaintenanceController::class, 'edit'])->name('maintenances.edit');
-Route::put('/maintenances/{maintenance}', [MaintenanceController::class, 'update'])->name('maintenances.update');
-Route::delete('/maintenances/{maintenance}', [MaintenanceController::class, 'destroy'])->name('maintenances.destroy');
-Route::get('/maintenances/download', [MaintenanceController::class, 'downloadPdf'])->name('maintenances.downloadPdf');
-Route::post('maintenances/hapusBulan', [MaintenanceController::class, 'hapusBulan'])->name('maintenances.hapusBulan');
 
 Route::middleware(['auth', 'role:marketing,admin,finance,warehouse,purchasing'])->group(function () {
     Route::get('/digital-marketing/list', [SuratMarketingController::class, 'list'])->name('surat.digital_marketing.list');
